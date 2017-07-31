4fe63a8fe923a0742e370df4ef201501b47a1097
ronanlima <ronan.saleslima@gmail.com>
2017-07-31 18:31:56 -0300

    - Inclusão do módulo Abstract Wizard Model. Será utilizado para criar os fragments com as perguntas após leitura de qr code.

---
+++ appWizards/src/main/java/com/markosullivan/wizards/wizard/TestActivity.java
@@ -0,0 +1,11 @@
+package com.markosullivan.wizards.wizard;
+
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Ronan.lima on 31/07/17.
+ */
+
+public class TestActivity extends FragmentActivity {
+}
