641bf45570e0a758378c9a2b00c7235e85ccdbfe
Ronan Sales Lima <ronan.lima@fornecedores.sicoob.com.br>
2018-08-13 07:25:49 -0300

    Revert "v1.0.19 (Desenvolvimento)"
    This reverts commit bf3f43b

--- app/src/main/java/br/com/home/maildeliveryjfsteel/persistence/impl/MailDeliveryDBContaNormal.java
+++ app/src/main/java/br/com/home/maildeliveryjfsteel/persistence/impl/MailDeliveryDBContaNormal.java
@@ -1,10 +1,13 @@
 package br.com.home.maildeliveryjfsteel.persistence.impl;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
 import java.util.List;
